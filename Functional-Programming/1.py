ec6b4cd3a27150c656d5393702192b3a1452271b
wangzhenqing <wangzhenqing1008@163.com>
2015-01-27 13:44:31 +0800

    add functiom

---
+++ Functional-Programming/1.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Wangzhenqing <wangzhenqing1008@163.com>
+# Date: 2015年01月27日13:13:29
+
+"""
+Problem 1: Implement a function product to multiply 2 numbers recursively using + and - operators only.
+"""
+
+
+def product(x, y):
+    if abs(x) > abs(y):
+        product(y, x)
+    res = 0
+    for i in range(abs(x)):
+        res += abs(y)
+    if (x > 0 and y > 0) or (x < 0 and y < 0):
+        return res;
+    return -res;
+
+print product(2, 3)
+print product(55, -32)
+print product(100, -1)
+
+"""
+Output
+6
+-1760
+-100
+"""
---
+++ Functional-Programming/2.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Wangzhenqing <wangzhenqing1008@163.com>
+# Date: 2015年01月27日13:21:17
+
+"""
+Problem 2: Write a function flatten_dict to flatten a nested dictionary by joining the keys with . character.
+"""
+
+
+def flatten_dict(b, a=None, result=None):
+    """Flattens a nested dict.
+
+        >>> flatten_dict({'a': 1, 'b': {'x': 2, 'y': 3}, 'c': 4})
+        {'a': 1, 'b.x': 2, 'b.y': 3, 'c': 4}
+    """
+    if result is None:
+        result = {}
+    for k,v in b.items():
+        if isinstance(v, dict):
+            flatten_dict(v, k, result)
+        else:
+            if a is None:
+                result[k] = v
+            else:
+                result[a+'.'+k] = v
+    return result
+
+
+res = flatten_dict({'a': 1, 'b': {'x': 2, 'y': 3}, 'c': 4})
+print res
+
+"""
+Output
+{'a': 1, 'c': 4, 'b.x': 2, 'b.y': 3}
+"""
